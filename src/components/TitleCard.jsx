75546f42de9a483e9f9c14636ae0ece3a3453e34
shaiksharzil <shaiksharzil@gmail.com>
2025-05-18 15:10:48 +0530

    css changed

--- src/components/TitleCard.jsx
+++ src/components/TitleCard.jsx
@@ -7,21 +7,17 @@ const TitleCard = ({ card, onEdit, onDelete }) => {
       <div className="flex flex-col h-40 w-70 border-2 rounded-md max-md:w-80">
         <div className="flex flex-row border-b-2 font-medium">
           <div
-            className="w-1/2 border-r-1 flex flex-row justify-center items-center gap-1 py-2 cursor-pointer"
+            className="group w-1/2 border-r-1 flex flex-row justify-center items-center gap-1 py-2 cursor-pointer"
             onClick={() => onEdit(card)}
           >
-            <div className="text-emerald-400">
-              <i className="ri-pencil-fill"></i>
-            </div>
+            <i className="ri-pencil-fill text-blue-400 transform transition-transform duration-500 group-hover:rotate-360"></i>
             <p>Edit</p>
           </div>
           <div
-            className="w-1/2 border-l-1 flex flex-row justify-center items-center gap-1 cursor-pointer"
+            className=" group w-1/2 border-l-1 flex flex-row justify-center items-center gap-1 cursor-pointer"
             onClick={() => onDelete(card)}
           >
-            <div className="text-red-500">
-              <i className="ri-delete-bin-6-line"></i>
-            </div>
+            <i className="ri-delete-bin-6-line text-red-500 transform transition-transform duration-200 group-hover:-rotate-12"></i>
             <p>Delete</p>
           </div>
         </div>
@@ -31,9 +27,9 @@ const TitleCard = ({ card, onEdit, onDelete }) => {
               {card.title}
             </h3>
             <Link to={`/tasks/${card._id}`} className="w-full">
-              <button className="flex group justify-center gap-1 bg-blue-500 w-full py-1 text-white text-xl rounded-md hover:bg-blue-400 active:scale-95 cursor-pointer">
+              <button className="flex group justify-center gap-1 border-2 border-black w-full py-1 text-xl rounded-md active:scale-95 cursor-pointer">
                 <p>Tasks</p>
-                <i className="ri-arrow-right-line transform transition-transform duration-200 group-hover:translate-x-1"></i>
+                <i className="ri-arrow-right-line transform transition-transform duration-200 group-hover:translate-x-1 text-emerald-500"></i>
               </button>
             </Link>
           </div>
